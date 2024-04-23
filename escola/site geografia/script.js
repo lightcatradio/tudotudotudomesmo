3111092608f37c4142a33d4e6bafba44c59e2308
Felipe M. Garcia <felipegarcia62@yahoo.com>
2024-04-23 18:54:20 -0300

    feito site geografia de uma vez so pq tenho preguica de ficar dando commit

---
+++ escola/site geografia/script.js
@@ -0,0 +1,54 @@
+// ---------- AMAZONIA ----------
+
+const popAmazonia = document.getElementById("pop-up-amazonia");
+function abreAmazonia() {
+  popAmazonia.classList.add("aparece");
+}
+function fechaAmazonia() {
+  popAmazonia.classList.remove("aparece");
+}
+// ---------- CAATINGA ----------
+
+const popCaatinga = document.getElementById("pop-up-caatinga");
+function abreCaatinga() {
+  popCaatinga.classList.add("aparece");
+}
+function fechaCaatinga() {
+  popCaatinga.classList.remove("aparece");
+}
+// ---------- CERRADO ----------
+
+const popCerrado = document.getElementById("pop-up-cerrado");
+function abreCerrado() {
+  popCerrado.classList.add("aparece");
+}
+function fechaCerrado() {
+  popCerrado.classList.remove("aparece");
+}
+// ---------- MATA ATLANTICA ----------
+
+const popMata = document.getElementById("pop-up-mata");
+function abreMata() {
+  popMata.classList.add("aparece");
+}
+function fechaMata() {
+  popMata.classList.remove("aparece");
+}
+// ---------- PAMPA ----------
+
+const popPampa = document.getElementById("pop-up-pampa");
+function abrePampa() {
+  popPampa.classList.add("aparece");
+}
+function fechaPampa() {
+  popPampa.classList.remove("aparece");
+}
+// ---------- PANTANAL ----------
+
+const popPantanal = document.getElementById("pop-up-pantanal");
+function abrePantanal() {
+  popPantanal.classList.add("aparece");
+}
+function fechaPantanal() {
+  popPantanal.classList.remove("aparece");
+}
