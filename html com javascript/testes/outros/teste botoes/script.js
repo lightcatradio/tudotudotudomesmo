1d51630f9e3abd0b2d6790542d267bc9c8e9c5ce
Felipe M. Garcia <felipegarcia62@yahoo.com>
2024-04-27 20:26:51 -0300

    testes formato botao

---
+++ html com javascript/testes/outros/teste botoes/script.js
@@ -0,0 +1,4 @@
+document.getElementById("arrowButton").addEventListener("click", function () {
+  // Adicione aqui o que deseja que aconteça quando o botão for clicado
+  console.log("Botão de seta clicado!");
+});
