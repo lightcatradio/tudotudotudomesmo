10696f956ac3b17876e09478e0682b3d662ff419
Felipe M. Garcia <felipegarcia62@yahoo.com>
2024-11-01 07:45:35 -0300

    ai que gostoso que delicia muito mais exercícios javascript

---
+++ outros/exemplos.js
@@ -0,0 +1,81 @@
+// =-=-=-=-=-=-=-=-=-= Exercício 1 =-=-=-=-=-=-=-=-=-=
+
+// let nome = "Felipe";
+// console.log(nome);
+
+// =-=-=-=-=-=-=-=-=-= Exercício 2 =-=-=-=-=-=-=-=-=-=
+
+// let primeiro_número = 24;
+// let segundo_número = 69;
+// console.log(
+//   `O primeiro número é ${primeiro_número}, o segundo número é ${segundo_número} e a soma dos dois é ${
+//     primeiro_número + segundo_número
+//   }!`
+// );
+
+// =-=-=-=-=-=-=-=-=-= Exercício 3 =-=-=-=-=-=-=-=-=-=
+
+// let variavel1 = 42;
+// let variavel2 = 94;
+// let variavel3 = 11;
+// let variavel4 = 83;
+// let variavel5 = 66;
+// let variavel6 = 76;
+// let variavel7 = 56;
+// let variavel8 = 20;
+// let variavel9 = 73;
+// let variavel10 = 8;
+
+// console.log(
+//   (variavel1 +
+//     variavel2 +
+//     variavel3 +
+//     variavel4 +
+//     variavel5 +
+//     variavel6 +
+//     variavel7 +
+//     variavel8 +
+//     variavel9 +
+//     variavel10) /
+//     10
+// );
+
+// =-=-=-=-=-=-=-=-=-= Exercício 4 =-=-=-=-=-=-=-=-=-=
+
+// let numero1 = 13;
+// let numero2 = 22;
+
+// console.log(
+//   `Entre os números ${numero1} e ${numero2}, temos as seguintes operações:
+//   Adição: ${numero1} + ${numero2} = ${numero1 + numero2}
+//   Subtração: ${numero1} - ${numero2} = ${numero1 - numero2}
+//   Multiplicação: ${numero1} x ${numero2} = ${numero1 * numero2}
+//   Divisão: ${numero1} / ${numero2} = ${(numero1 / numero2).toFixed(2)}`
+// );
+
+// =-=-=-=-=-=-=-=-=-= Exercício 5 =-=-=-=-=-=-=-=-=-=
+
+// let idade = 17.99;
+
+// if (idade >= 18) {
+//   console.log(`Você tem ${idade} anos, e isso te faz maior de idade!`);
+// } else {
+//   console.log(`Você tem ${idade} anos, e isso te faz menor de idade...`);
+// }
+
+// =-=-=-=-=-=-=-=-=-= Exercício 6 =-=-=-=-=-=-=-=-=-=
+
+// let numero1 = 1;
+// let numero2 = 999999999999999;
+
+// console.log(
+//   `O maior número entre ${numero1} e ${numero2} é ${Math.max(numero1, numero2)}`
+// );
+
+// =-=-=-=-=-=-=-=-=-= Exercício 7 =-=-=-=-=-=-=-=-=-=
+
+// let diametro = 10;
+// let circunferencia = Math.PI * diametro;
+// console.log(`O valor é: ${circunferencia.toFixed(2)}`);
+
+// =-=-=-=-=-=-=-=-=-= Exercício 8 =-=-=-=-=-=-=-=-=-=
