69b0f3b47a6a3e5becf4e9d6d958ac2f6a8fe292
Felipe M. Garcia <felipegarcia62@yahoo.com>
2024-11-16 16:57:34 -0300

    mããããeeee, termineeeeiiii
    vem me limpar

--- escola/avaliacoes/site natal/script.js
+++ escola/avaliacoes/site natal/script.js
@@ -1,19 +1,25 @@
 let slideIndex = 0;
-showSlides();
+let slides = document.getElementsByClassName("slide");
+showSlides(slideIndex);
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
+function showSlides(slideIndex) {
+  let container = document.getElementsByClassName("slides")[0];
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slideIndex++;
-
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000);
+  console.log(`SHOW: length: ${slides.length}, slideIndex: ${slideIndex}`);
+  container.style.transform = `translateX(-${slideIndex * 100}%)`;
 }
+
+function changeSlide(change) {
+  slideIndex += change;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  console.log(`CHANGE: change: ${change}, slideIndex: ${slideIndex}`);
+  showSlides(slideIndex);
+}
+
+setInterval(() => {
+  changeSlide(1);
+}, 5000);
